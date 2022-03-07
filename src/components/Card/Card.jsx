13909f906c619d09f216988d27acc45edc15f029
Razvan Borsan <razvanborsan@gmail.com>
2022-03-08 00:14:11 +0200

    Fixed pokemon card colors issue

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import CardHeader from './CardHeader/CardHeader';
 import CardBody from './CardBody/CardBody';
 
+import '../../shared/pokemonTypes.css';
 import './Card.css';
 
 function Card({ pokemon }) {
   return (
-    <div className="cardContainer" style={{ backgroundColor: pokemon.color }}>
+    <div className={`cardContainer ${pokemon.types[0].type.name}`}>
       <CardHeader pokemon={pokemon} />
       <CardBody pokemon={pokemon} />
     </div>
