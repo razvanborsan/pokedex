ddcfcf7783fc2f3146d916cf2d37f0278abe9c19
Razvan Borsan <razvanborsan@gmail.com>
2022-04-28 13:52:22 +0300

    Added header + github footer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,29 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Image } from '@chakra-ui/react';
+
+import githubLogo from 'images/githubLogo.svg';
+import pokedexTitle from 'images/pokedexTitle.svg';
 
 import './Layout.css';
 
 function Layout({ children }) {
   return (
-    <div className="app-container">
-      <div className="main-content">
-        <Link to="/pokemon">
-          {/* <h1 className="pokedex-title">Pokedex</h1> */}
-          <Text
-            className="pokedex-title"
-            fontSize="3xl"
+    <Flex
+      justify="center"
+      align="center"
+      direction="column"
+      className="app-container"
+    >
+      <Flex
+        justify="center"
+        align="center"
+        direction="column"
+        className="main-content"
+      >
+        <Link
+          to="/pokemon"
+          className="title-container"
+        >
+          <Flex
+            justify="center"
+            align="center"
+            direction="row"
+            marginBottom="20px"
+            width="1080px"
           >
-            Pokedex
-          </Text>
+            <Image
+              src={pokedexTitle}
+              alt="Pokedex title"
+              height="125px"
+            />
+          </Flex>
         </Link>
 
         <Flex justify="center" align="flex-start" direction="column">
           {children}
         </Flex>
-      </div>
-    </div>
+      </Flex>
+
+      <Flex
+        justify="center"
+        align="center"
+        direction="row"
+        width="100%"
+        margin="20px"
+        height="100px"
+      >
+        <a href="https://github.com/razvanborsan" target="_blank" rel="noreferrer">
+          <Image
+            src={githubLogo}
+            alt="Github icon"
+            boxSize="65px"
+          />
+        </a>
+      </Flex>
+    </Flex>
   );
 }
 
